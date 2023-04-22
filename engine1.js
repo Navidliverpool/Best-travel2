bfcea65294580c2faaeeb0e23d9fdbf98f28bfa4
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-22 15:48:34 +0100

    engine1 added2.

--- engine1.js
+++ engine1.js
@@ -1,3 +1,14 @@
-function chooseBestSum(t, k, ls) {
-
+// This function creates subarrays from ls and store them in an array. 
+function chooseBestSum(k, ls) {
+    let slicedLs = [];
+    let a = 0;
+    let b = k;
+    for (let i = 0; i < ls.length; i += 3) {
+        slicedLs[a] = ls.slice(i, b);
+        b += 3;
+        a++;
+    }
+    console.log(slicedLs)
 }
+let ts = [50, 55, 56, 57, 58, 59];
+console.log(chooseBestSum(3, ts))
