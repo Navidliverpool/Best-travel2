e1cd9bc357d88c9ca9dae8c736ab952d773c843e
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-23 02:53:09 +0100

    engine5.js name has been changed tho engine2.js after removing some js files.

--- engine2.js
+++ engine2.js
@@ -1,24 +1,30 @@
+// This function finds the three biggest numbers in ls and store them in arr variable.
+// This method does not solve the pseudocode.
 function chooseBestSum(ls) {
-    let a = 0;
     let arr = [];
     let b = ls[0];
-    // for (let i = 0; i < ls.length; i += 3) {
-    //     if (ls[i] > ls[0]) {
-    //         b = ls[i];
-    //     }
-    // }
-    let c = 3;
-    let j = 0;
+    let c = 0;
+    let j = 1;
+    let index = '';
     // c should be replaced with k.
-    while (c <= 3) {
-        if (ls[j] > ls[0]) {
+    while (c < 3) {
+        if (ls[j] > b) {
             b = ls[j];
+            console.log("b:", b)
         }
         j++;
-        if (j == ls.length) {
-            arr[c] = b;
-            b = 0;
+        if (j >= ls.length) {
+            console.log("j:", j)
+            arr.push(b);
+            console.log("arr:", arr)
+            index = ls.indexOf(b);
+            if (index > -1) { // only splice array when item is found
+                ls.splice(index, 1); // 2nd parameter means remove one item only
+            }
+            b = ls[0];
+            console.log("ls:", ls)
             c++;
+            j = 1;
         }
     }
     return arr;
